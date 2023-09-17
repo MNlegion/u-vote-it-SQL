cac11e32cb63bb267b2506dda9ba2b382b50d001
MNlegion <rkreuser30@gmail.com>
2023-09-16 21:19:41 -0400

    db query for select all

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 
 // Express middleware
 
 app.use(express.urlencoded({ extended:false }));
 app.use(express.json());
 
-// Get test route
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
+// Connect to Database
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '0206192819891988!Baron',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
+
+
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
 
 // Default response for any other NOT FOUND requests
